ce5f84e1db6ae6f668624cbf97c5c1fc7cea9f89
A Student <student@nowhere.edu>
2025-10-27 07:37:17 +0000

    Oct 27, 2025, 12:37 AM

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ double getShippingCost(int ShippingMethod, double cost_of_order) {
     else {
       ShippingCost = cost_of_order * 0.1;
     }
-                    }
+  }
                      else if (ShippingMethod == 2) {
                             cout << "2 day delivery" << endl;
                                       if (cost_of_order <= 300) {
