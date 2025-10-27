879877e45b6f56611d8a653e15386e4aab5ca43c
A Student <student@nowhere.edu>
2025-10-27 08:57:57 +0000

    Oct 27, 2025, 1:57 AM

--- main.cpp
+++ main.cpp
@@ -89,13 +89,13 @@ int main() {
 
     double ShippingCost = getShippingCost(ShippingMethod, cost_of_order);
 
-         if (ShippingCost = -1) {
-            cout << "Not available" << endl;
-           }
-           else if (ShippingCost = -2) {
-                                  cout << "Free" << endl;
-                              }                                                                                                                                                                                                                               else {
-                                                                                                                                                                                                                                                                                                                  cout << "Total is: " << cost_of_order + ShippingCost << endl;
+      if (ShippingCost = -1) {
+        cout << "Not available" << endl;
+      }
+      else if (ShippingCost = -2) {
+        cout << "Free" << endl;
+      }                                                                                                                                                                                                                               else {
+      else << "Total is: " << cost_of_order + ShippingCost << endl;
 ///----------------------------------------------------------------------------------
 /// Tests
 /// Uncomment tests to work on them. Make sure any test that does not compile or
