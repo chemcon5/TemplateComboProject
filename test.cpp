33770bd9108f5c9f1d7f16d5d4c71310b87ad57e
A Student <student@nowhere.edu>
2025-10-27 04:45:38 +0000

    Oct 26, 2025, 9:45 PM

--- test.cpp
+++ test.cpp
@@ -59,7 +59,7 @@ double getShippingCost(int ShippingMethod, double cost_of_order) {
    }
 }
 
-
+int main
 ///----------------------------------------------------------------------------------
 /// Tests
 /// Uncomment tests to work on them. Make sure any test that does not compile or
