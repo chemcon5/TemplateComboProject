ebd58b22b9bf5ad055a87097a1bb402069a01c25
A Student <student@nowhere.edu>
2025-10-27 05:38:25 +0000

    Oct 26, 2025, 10:38 PM

--- test.cpp
+++ test.cpp
@@ -54,7 +54,7 @@ double getShippingCost(int ShippingMethod, double cost_of_order) {
       else if (cost_of_order = 35 && cost_of_order < 100) {
          ShippingCost = 8;
       }
-      else (cost_of_order => 100) {
+      else {
          cout << "Free" << endl;
       }
    }
