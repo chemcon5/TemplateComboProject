9a1fcc9c31b82d99b3fc8e7f70709c126671eb7a
A Student <student@nowhere.edu>
2025-10-27 06:34:11 +0000

    Oct 26, 2025, 11:34 PM

--- main.cpp
+++ main.cpp
@@ -78,9 +78,10 @@ int main() {
 
    cout << "Enter your Shipping Method: "<< endl; 
    cin >> ShippingMethod;
+
    if (ShippingMethod < 1 || ShippingMethod > 3)) {
     cout << "Bad Input" << endl;
-        return 1; 
+    return 1; 
    }
 
    double ShippingCost = getShippingCost(ShippingMethod, cost_of_order);
